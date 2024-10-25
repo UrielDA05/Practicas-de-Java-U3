11b88ac8c0396a28712283fd97eb38edc7becabc
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-10-25 09:52:33 -0600

    Correccion de mensaje

--- src/Monto_Talleres.java
+++ src/Monto_Talleres.java
@@ -1,21 +1,9 @@
 
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author uriel
- */
 public class Monto_Talleres {
     public static void main(String[] args) {
-        double basket , voli ,artes ,fut ,taek ,aje ,mus ,atle ;
-        short mBask=9000 , mVoli=15000, mFut=15000;
-        short mAje=25000 , mMus=12000 , mAtl=13000 ;
-        int montoT=189000, mTaek=50000, mArt=50000;
+        float basket , voli ,artes ,fut ,taek ,aje ,mus ,atle ,mBask=9000 , mVoli=15000, mFut=15000, mAje=25000 , mMus=12000 , mAtl=13000, montoT=189000, mTaek=50000, mArt=50000;
         basket = (mBask/(montoT/100));
         voli = (mVoli/(montoT/100));
         artes= (mArt/(montoT/100));
@@ -24,14 +12,16 @@ public class Monto_Talleres {
         aje=(mAje/(montoT/100));
         mus=(mMus/(montoT/100));
         atle=(mAtl/(montoT/100));
-        JOptionPane.showMessageDialog(null,"Taller basketball: $"+mBask+", porcentaje: %"+basket);
-        JOptionPane.showMessageDialog(null,"Taller voliball: $"+mVoli+", porcentaje: %"+voli);
-        JOptionPane.showMessageDialog(null,"Taller artes: $"+mArt+", porcentaje: %"+artes);
-        JOptionPane.showMessageDialog(null,"Taller futball: $"+mFut+", porcentaje: %"+fut);
-        JOptionPane.showMessageDialog(null,"Taller taekwondo: $"+mTaek+", porcentaje: %"+taek);
-        JOptionPane.showMessageDialog(null,"Taller ajedrez: $"+mAje+", porcentaje: %"+aje);
-        JOptionPane.showMessageDialog(null,"Taller musica: $"+mMus+", porcentaje: %"+mus);
-        JOptionPane.showMessageDialog(null,"Taller atletismo: $"+mAtl+", porcentaje: %"+atle);
+        String resultado=
+        "Taller basketball: $"+mBask+", porcentaje: "+basket+"%\n"+
+        "Taller voliball: $"+mVoli+", porcentaje: "+voli+"%\n"+
+        "Taller artes: $"+mArt+", porcentaje: "+artes+"%\n"+
+        "Taller taekwondo: $"+mTaek+", porcentaje: "+taek+"%\n"+
+        "Taller ajedrez: $"+mAje+", porcentaje: "+aje+"%\n"+
+        "Taller musica: $"+mMus+", porcentaje: "+mus+"%\n"+
+        "Taller atletismo: $"+mAtl+", porcentaje: "+atle+"%";
+        JOptionPane.showMessageDialog(null,resultado);
+        
         
         
         
