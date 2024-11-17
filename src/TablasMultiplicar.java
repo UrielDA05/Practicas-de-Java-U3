50b9b13f94bb9f918a88d5cacc356c8718b62a2d
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-16 22:34:45 -0600

    Uso de for

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -1,21 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author uriel
- */
 public class TablasMultiplicar {
     public static void main(String[] args) {
-        for(int a=1; a<=10;a++){
-            System.out.println("Tabla del : "+a );
-            for(int b=1;b<=10;b++){
-                int r=a*b;
-                System.out.println(a+"*"+b+"="+r);
+        int num = 10;
+        for(int a=1; a<=num;a++){
+            System.out.printf(("Tabla del : "+ a + "\t" ));
+        }    
+        System.out.println("");
+        for (int y = 1; y <= num; y++) {
+            for (int x = 1; x <= num; x++) {
+                System.out.printf(x + " x " + y + " = " + (x * y) + "\t");
             }
-            System.out.println("");
+            System.out.println();
         }
     }      
 }
