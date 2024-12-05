d9903827d6f4d74ddaed4a2ed13a396b788b4317
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-04 22:56:51 -0600

    Update Descuento_tombola.java

--- src/Descuento_tombola.java
+++ src/Descuento_tombola.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import javax.swing.JOptionPane;
+import javax.swing.JOptionP
 public class Descuento_tombola {
     public static void main(String[] args) {
         double precioI , precioF ,descuento;
